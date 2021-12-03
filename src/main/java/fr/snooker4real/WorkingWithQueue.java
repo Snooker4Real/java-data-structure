1d7c2f737d4021ef0579204ce412c18d479e9f19
Cindano Jonathan <cindanojonathan@icloud.com>
2021-12-03 17:12:14 +0100

    linkedList added

--- src/main/java/fr/snooker4real/WorkingWithQueue.java
+++ src/main/java/fr/snooker4real/WorkingWithQueue.java
@@ -1,12 +1,30 @@
 package fr.snooker4real;
 
 import java.util.LinkedList;
+import java.util.ListIterator;
 import java.util.Queue;
 
 public class WorkingWithQueue {
     public static void main(String[] args) {
         // Queue : first in, first out FIFO
 
+        LinkedList<Person> linkedList = new LinkedList<>();
+        linkedList.add(new Person("Alex", 21));
+        linkedList.add(new Person("Alexa", 21));
+        linkedList.addFirst(new Person("Ali", 18));
+
+        ListIterator<Person> personListIterator = linkedList.listIterator();
+        while (personListIterator.hasNext()){
+            System.out.println(personListIterator.next());
+        }
+        System.out.println();
+        while (personListIterator.hasPrevious()){
+            System.out.println(personListIterator.previous());
+        }
+
+    }
+
+    private static void queues() {
         Queue<Person> supermarket = new LinkedList<>();
         supermarket.add(new Person("Alex", 21));
         supermarket.add(new Person("Mariam", 18));
@@ -16,7 +34,6 @@ public class WorkingWithQueue {
         System.out.println(supermarket.poll()); // removes it from the queue
         System.out.println(supermarket.size());
         System.out.println(supermarket.peek());
-
     }
 
     static record Person(String name, int age){}
