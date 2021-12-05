1ccfd8a4404dcd302ea1b54465441f1a99bc4c7a
Snooker4real <cindanojonathan@icloud.com>
2021-12-05 12:34:01 +0100

    adding things in maps

--- src/main/java/fr/snooker4real/WorkingWithMaps.java
+++ src/main/java/fr/snooker4real/WorkingWithMaps.java
@@ -9,13 +9,23 @@ public class WorkingWithMaps {
         map.put(1, new Person("Alex"));
         map.put(2, new Person("Alexa"));
         map.put(3, new Person("Mariam"));
-        map.put(3, new Person("Mariam 2"));
 
         System.out.println(map);
         System.out.println(map.size());
         System.out.println(map.get(1));
         System.out.println(map.containsKey(4));
         System.out.println(map.entrySet());
+        System.out.println(map.keySet());
+        map.remove(3);
+
+        map.entrySet()
+                .forEach(x -> System.out.println(x.getKey() + " " + x.getValue()));
+
+        map.forEach(((key, person) -> {
+            System.out.println(key + " " + person);
+        }));
+        System.out.println(map.getOrDefault(3, new Person("default")));
+        System.out.println(map.values());
         System.out.println(map.keySet());
     }
 
