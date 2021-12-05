3649621dfcb4a173adfab7aa6303b929e291dc62
Snooker4real <cindanojonathan@icloud.com>
2021-12-05 12:25:46 +0100

    adding maps

---
+++ src/main/java/fr/snooker4real/WorkingWithMaps.java
@@ -0,0 +1,23 @@
+package fr.snooker4real;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WorkingWithMaps {
+    public static void main(String[] args) {
+        Map<Integer, Person> map = new HashMap<>();
+        map.put(1, new Person("Alex"));
+        map.put(2, new Person("Alexa"));
+        map.put(3, new Person("Mariam"));
+        map.put(3, new Person("Mariam 2"));
+
+        System.out.println(map);
+        System.out.println(map.size());
+        System.out.println(map.get(1));
+        System.out.println(map.containsKey(4));
+        System.out.println(map.entrySet());
+        System.out.println(map.keySet());
+    }
+
+    record Person(String name){}
+}
